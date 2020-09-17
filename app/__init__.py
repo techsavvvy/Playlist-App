8d37b0d1e23fd32b248e139b155dcd1f1baa8ffc
sasidharan velu <sasidharan@entrayn.com>
2020-09-17 22:34:19 +0530

    feat: setup user management login/signup flow for the app

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from app.usermgmt.views import usermgmt
+app.register_blueprint(usermgmt, url_prefix='/')
---
+++ app/usermgmt/views.py
@@ -0,0 +1,63 @@
+from app.usermgmt.forms import LoginForm, RegisterForm
+from app.usermgmt.models import User
+from app import db
+from app import app
+
+from flask import Blueprint
+from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+usermgmt=Blueprint('usermgmt', __name__, url_prefix='/')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@usermgmt.route('/')
+def index():
+    return render_template('index.html')
+
+@usermgmt.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('playlist.dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+
+    return render_template('login.html', form=form)
+
+@usermgmt.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+
+    return render_template('signup.html', form=form)
+
+@usermgmt.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('usermgmt.index'))
